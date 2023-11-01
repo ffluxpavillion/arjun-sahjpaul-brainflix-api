fffb4699509c0bfafc3658310ab55588a8c05384
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-11-01 03:09:03 -0700

    fixed publish videos, uuid, image, random names, comments + more

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,106 +1,106 @@
 const express = require("express");
-const fs = require('fs');
+const fs = require('fs').promises; // use fs promises for async operations
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
+const uuidv4 = require('uuid');
 const cors = require('cors');
-const path = require('path');
-const { generateName } = require("../helper/name");
+router.use(cors());
+router.use(express.json());
+const { generateRandomName } = require('../helper/name');
 
+const videosFilePath = './data/video-details.json';
 
-
-// middleware to handle json data + cors
-router.use(cors())
-router.use(express.json())
-
-const videosFilePath = path.join(__dirname, '../data/video-details.json');
-
-// function to read the json file
-function readVideosFile() {
-    return JSON.parse(fs.readFileSync(videosFilePath, 'utf8'));
+// function to read the JSON file
+async function readVideosFileAsync() {
+    try {
+        const fileContent = await fs.readFile(videosFilePath, 'utf8');
+        return JSON.parse(fileContent);
+    } catch (error) {
+        console.error('Error reading file:', error);
+        throw new Error('Error reading video data');
+    }
 }
 
-// GET /videos endpoint - responds with an array of videos
-router.get('/', (req, res) => {
+// function to write to the JSON file
+async function writeVideosFileAsync(data) {
+    try {
+        await fs.writeFile(videosFilePath, JSON.stringify(data, null, 2));
+    } catch (error) {
+        console.error('Error writing file:', error);
+        throw new Error('Error writing video data');
+    }
+}
+
+router.get('/', async (req, res) => {
   try {
-      const videos = readVideosFile();
-      res.json(videos);
+      const videos = await readVideosFileAsync();
+      res.json(videos.map(({ comments, ...video }) => video));
   } catch (error) {
-    console.error('Error occurred in GET /videos:', error);
-    res.status(500).send('Error reading video data'); 
+    res.status(500).send(error.message);
   }
 });
 
-// GET /videos/:id endpoint - responds with object containing video details by id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-      const videos = readVideosFile();
-      const video = videos.find((video) => video.id === req.params.id);
-      if (video) {
-          res.json(video);
-      } else {
-          res.status(404).send('Video not found');
-      }
+      const videos = await readVideosFileAsync();
+      const video = videos.find(video => video.id === req.params.id);
+
+      // Check if video is found, and include comments in the response
+      video ? res.json(video) : res.status(404).send('Video not found');
   } catch (error) {
-    console.error('Error occurred in GET /videos/:id:', error);
-    res.status(500).send('Error reading video data');
+    res.status(500).send(error.message);
   }
 });
 
-// POST /videos endpoint - Adds a new video to list with unique id
-router.post('/', (req, res) => {
+
+
+router.post('/', async (req, res) => {
   try {
-      const { title, description, image } = req.body;
-      if (!title || !channel) {
-        // bad request - need title and channel
-        return res.status(400).json({
-          error:
-            "Please provide video title and channel for uploading a new video.",
-        });
+      const { title, description } = req.body;
+      if (!title || !description) {
+        return res.status(400).json({ error: "Please provide video title and description." });
       }
-    
-      const videos = readVideosFile();
-      const newVideo = {
-          id: uuidv4(),
-          title,
-          channel: generateName(),
-          image,
-          description: description,
-          views: "0",
-          likes: "0",
-          duration: "2:00",
-          video: "https://project-2-api.herokuapp.com/stream",
-          timestamp: Date.now(),
-          comments:[
-            {
-              "id": uuidv4(),
-              "name": generateName(),
-              "comment": "Absolutely spot on with the emphasis on location! It’s incredible how pivotal it is for a travel experience. Your insights have inspired me; I’m now envisioning waking up to ocean waves on my next trip!",
-              "likes": 4,
-              "timestamp": 1631816492000
-            } ,
-            {
-              "id": uuidv4(),
-              "name": generateName(),
-              "comment": "I’m so tempted to print this out for my next travel planning session, but then again, digital is the way to go! Your tips are golden; can't wait to try them out on my upcoming journey.",
-              "likes": 3,
-              "timestamp": 1631799181000
-            },
-            {
-              "id": uuidv4(),
-              "name": generateName(),
-              "comment": "Whether it’s luxury or budget-friendly, your approach to travel accommodation is spot on! Give me a serene view and a comfortable bed over extravagant amenities any day. Excited to try this approach!",
-              "likes": 2,
-              "timestamp": 1631716921000
-            }            
-          ]
-      };
+
+      const videos = await readVideosFileAsync();
+      const newVideo = createNewVideo(req.body);
       videos.push(newVideo);
-      fs.writeFileSync(videosFilePath, JSON.stringify(videos, null, 2));
+      await writeVideosFileAsync(videos);
       res.status(201).send(newVideo);
   } catch (error) {
-    console.error('Error occurred in POST /videos:', error);
-    res.status(500).send('Error saving video data');
+    res.status(500).send(error.message);
   }
 });
+
+function createNewVideo({ title, description }) {
+    return {
+        id: uuidv4.v4(),
+        title: title,
+        channel: "ChannelName",
+        image: "http://localhost:8080/Upload-video-preview.jpg",
+        timestamp: Date.now(),
+        comments: [
+          {
+            "id": uuidv4.v4(),
+            "name": generateRandomName(),
+            "comment": "Absolutely spot on with the emphasis on location! It’s incredible how pivotal it is for a travel experience. Your insights have inspired me; I’m now envisioning waking up to ocean waves on my next trip!",
+            "likes": 4,
+            "timestamp": 1631816492000
+          },
+          {
+            "id": uuidv4.v4(),
+            "name": generateRandomName(),
+            "comment": "I’m so tempted to print this out for my next travel planning session, but then again, digital is the way to go! Your tips are golden; can't wait to try them out on my upcoming journey.",
+            "likes": 3,
+            "timestamp": 1631799181000
+          },
+          {
+            "id": uuidv4.v4(),
+            "name": generateRandomName(),
+            "comment": "Whether it’s luxury or budget-friendly, your approach to travel accommodation is spot on! Give me a serene view and a comfortable bed over extravagant amenities any day. Excited to try this approach!",
+            "likes": 2,
+            "timestamp": 1631716921000
+          }
+        ]
+    };
+}
 
 module.exports = router;
